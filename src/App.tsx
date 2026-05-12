652031d3b2b971d6d3a477f812ffec25464f8b0a
Zaheer Ahmed <zaheerahmed.ite@gmail.com>
2026-05-12 16:58:54 +0530

    update default open page from starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
@@ -10,10 +11,21 @@ import { Learning } from './pages/Learning';
 import { Contact } from './pages/Contact';
 import { Services } from './pages/Services';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-white">
+        <ScrollToTop />
         <Navbar />
         <main className="flex-grow">
           <Routes>
